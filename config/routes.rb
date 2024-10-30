00d8b9b277fd4e1cf57a1bac9685ea9c574f8943
qinmingyuan <mingyuan0715@foxmail.com>
2024-10-31 01:44:10 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 Rails.application.routes.draw do
+  scope RailsCom.default_routes_scope do
+    namespace :alipay, defaults: { business: 'alipay' } do
+      namespace :admin, defaults: { namespace: 'admin' } do
+        
+      end
+    end
+  end
 end
